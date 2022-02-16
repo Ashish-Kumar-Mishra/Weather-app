5a98807bbfb3f57ff78e52be9ce09d1241e0d47e
Ashish-Kumar-Mishra <95560382+Ashish-Kumar-Mishra@users.noreply.github.com>
2022-02-16 21:33:35 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,68 +1,10 @@
 
-// if (document.referrer != '') {
-//     alert('Thanks for visiting from ' + document.referrer);
-// }
-// else {
-//     alert('Welcome To My Website, Thanks for visiting.')
-// }
-
-// console.log("it is weather page");
-// const cityName = document.getElementById('cityName');
-// const submitBtn = document.getElementById('submitBtn');
-// const city_name = document.getElementById('city_name');
-// const temp = document.getElementById('temp')
-// const temp_status = document.getElementById('temp_status')
-
-
-// const getInfo = async (event) => {
-//     event.preventDefault();
-//     let cityVal = cityName.value;
-//     if (cityVal === "") {
-//         city_name.innerText = `Please write the city name before search`
-//     }
-//     else {
-//         try {
-//             let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=7a1719c75904e05cad85361c948c86e9`
-//             const response = await fetch(url);
-//             const data = await response.json()
-//             // console.log(data);
-//             const arrData = [data];
-//             city_name.innerText = `${arrData[0].name}.${arrData[0].sys.country}`;
-
-//             temp.innerText = arrData[0].main.temp;
-//             const tempMood = arrData[0].weather[0].main;
-
-//              //condition to check sunny or cloudy
-//              if (tempMood == "Clear") {
-//                 temp_status.innerHTML =
-//                     "<i class='fas fa-sun' style='color: #eccc68;'></i>";
-//                 } else if (tempMood == "Clouds") {
-//                 temp_status.innerHTML =
-//                     "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
-//                 } else if (tempMood == "Rain") {
-//                 temp_status.innerHTML =
-//                     "<i class='fas fa-cloud-rain' style='color: #a4b0be;'></i>";
-//                 } else {
-//                 temp_status.innerHTML =
-//                     "<i class='fas fa-cloud' style='color:#f1f2f6;'></i>";
-    
-//                 }
-//                 datahide.classList.remove('data_hide');
-//                 cityVal = "";
-
-//         } catch {
-//             city_name.innerText = `${cityVal}`
-            
-//         }
-//     }
-
-// }
-
-// submitBtn.addEventListener('click', getInfo);
-
-
-
-
+if (document.referrer != '') {
+    alert('Thanks for visiting from ' + document.referrer);
+}
+else {
+    alert('Welcome To My Website, Thanks for visiting.')
+}
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
 
@@ -84,7 +26,9 @@ const getInfo = async(event) => {
         try{
            
 
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=7a1719c75904e05cad85361c948c86e9`
+//             let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=7a1719c75904e05cad85361c948c86e9`
+                      let url=` https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=7a1719c75904e05cad85361c948c86e9&units=metric`
+
             const response = await fetch(url);
 
             const data = await response.json();
